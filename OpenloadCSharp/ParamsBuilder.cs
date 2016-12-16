b2fb74fedb0f54b2a2994c649ecc7a19ada2f170
Simon Haga <simon.haga94@gmail.com>
2016-12-16 03:36:31 +0100

    Initial commit.

---
+++ OpenloadCSharp/ParamsBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace OpenloadCSharp {
+	class ParamsBuilder: Dictionary<string, object> {
+		public override string ToString() {
+			string parameters = "";
+			bool first = true;
+			foreach (var parameter in this) {
+				if (parameter.Value != null) {
+					if (first) {
+						first = false;
+					} else {
+						parameters += "&";
+					}
+
+					parameters += $"{parameter.Key}={parameter.Value}";
+				}
+
+			}
+
+			return parameters;
+		}
+	}
+}
