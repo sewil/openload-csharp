6ee56bd8e929146079bcb46f3760685d3d17d4ab
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-11-11 01:02:34 +0100

    Make Openload login and key non-static

--- OpenloadCSharp/Openload.cs
+++ OpenloadCSharp/Openload.cs
@@ -6,73 +6,72 @@ namespace OpenloadCSharp
 {
     public class Openload
     {
-        private static string login;
-        private static string key;
+        private string login;
+        private string key;
 
-        public static AccountInfoResponse Authenticate(string login, string key)
+        public Openload(string login, string key)
         {
-            Openload.login = login;
-            Openload.key = key;
-
-            return AccountInfo();
-        }
-        public static void Deauthenticate()
-        {
-            login = null;
-            key = null;
-        }
-        public static bool Authenticated()
-        {
-            return login != null && key != null;
+            this.login = login;
+            this.key = key;
         }
 
-        public static AccountInfoResponse AccountInfo()
-        {
-            return Request<AccountInfoResponse>($"account/info?login={login}&key={key}");
-        }
-        public static DownloadTicketResponse DownloadTicket(string file)
-        {
-            return Request<DownloadTicketResponse>($"file/dlticket?{ new ParamsBuilder { { "file", file }, { "login", login }, { "key", key } } }");
-        }
         public static DownloadLinkResponse DownloadLink(string file, string ticket, string captcha_response)
         {
             return Request<DownloadLinkResponse>($"file/dl?{ new ParamsBuilder { { "file", file }, { "ticket", ticket }, { "captcha_response", captcha_response } } }");
         }
+
         public static DownloadLinkResponse DownloadLink(string file, string ticket)
         {
-
             return Request<DownloadLinkResponse>($"file/dl?{ new ParamsBuilder { { "file", file }, { "ticket", ticket } } }");
         }
-        public static FileInfoResponse FileInfo(params string[] file)
+
+        public AccountInfoResponse AccountInfo()
+        {
+            return Request<AccountInfoResponse>($"account/info?login={login}&key={key}");
+        }
+
+        public DownloadTicketResponse DownloadTicket(string file)
+        {
+            return Request<DownloadTicketResponse>($"file/dlticket?{ new ParamsBuilder { { "file", file }, { "login", login }, { "key", key } } }");
+        }
+
+        public FileInfoResponse FileInfo(params string[] file)
         {
             string fileids = string.Join(",", file);
             return Request<FileInfoResponse>($"file/info?{ new ParamsBuilder { { "file", fileids }, { "login", login }, { "key", key } } }");
         }
-        public static UploadResponse Upload(int? folder = null, string sha1 = null, string httponly = null)
+
+        public UploadResponse Upload(int? folder = null, string sha1 = null, string httponly = null)
         {
             return Request<UploadResponse>($"file/ul?{ new ParamsBuilder { { "login", login }, { "key", key }, { "folder", folder }, { "sha1", sha1 }, { "httponly", httponly } } }");
         }
-        public static AddRemoteUploadResponse AddRemoteUpload(string url, int? folder = null, string headers = null)
+
+        public AddRemoteUploadResponse AddRemoteUpload(string url, int? folder = null, string headers = null)
         {
             return Request<AddRemoteUploadResponse>($"remotedl/add?{ new ParamsBuilder { { "login", login }, { "key", key }, { "url", url }, { "folder", folder }, { "headers", headers } } }");
         }
-        public static CheckRemoteUploadResponse CheckRemoteUpload(int? limit = null, int? id = null)
+
+        public CheckRemoteUploadResponse CheckRemoteUpload(int? limit = null, int? id = null)
         {
             return Request<CheckRemoteUploadResponse>($"remotedl/status?{ new ParamsBuilder { { "login", login }, { "key", key }, { "limit", limit }, { "id", id } } }");
         }
-        public static ListFolderResponse ListFolder(int? folder = null)
+
+        public ListFolderResponse ListFolder(int? folder = null)
         {
             return Request<ListFolderResponse>($"file/listfolder?{ new ParamsBuilder { { "login", login }, { "key", key }, { "folder", folder } } }");
         }
-        public static ConvertingFilesResponse ConvertingFiles(string file)
+
+        public ConvertingFilesResponse ConvertingFiles(string file)
         {
             return Request<ConvertingFilesResponse>($"file/convert?{ new ParamsBuilder { { "login", login }, { "key", key }, { "file", file } } }");
         }
-        public static RunningConvertsResponse RunningConverts(int? folder = null)
+
+        public RunningConvertsResponse RunningConverts(int? folder = null)
         {
             return Request<RunningConvertsResponse>($"file/listfolder?{ new ParamsBuilder { { "login", login }, { "key", key }, { "folder", folder } } }");
         }
-        public static GetSplashResponse GetSplash(string file)
+
+        public GetSplashResponse GetSplash(string file)
         {
             return Request<GetSplashResponse>($"file/convert?{ new ParamsBuilder { { "login", login }, { "key", key }, { "file", file } } }");
         }
